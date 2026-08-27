99ef4ceabaa000d11735445ba5b682aab39a3825
Praveen S <shanupraveen1993@gmail.com>
2026-08-27 15:26:00 +0530

    fix: remove duplicate standalone UniversalSearchBarRow from sub-pages

--- src/app/(main)/shops/ShopsClientPage.tsx
+++ src/app/(main)/shops/ShopsClientPage.tsx
@@ -73,7 +73,6 @@ export default function ShopsClientPage() {
 
   return (
     <div className="flex flex-col gap-0 pb-6 sm:pb-10 w-full font-sans">
-      <UniversalSearchBarRow />
       <HomeCategorySegmentBar />
 
       <div className="w-full max-w-7xl mx-auto px-3 sm:px-6 flex flex-col gap-3 mt-2">
